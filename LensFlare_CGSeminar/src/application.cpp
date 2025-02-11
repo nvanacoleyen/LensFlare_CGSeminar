d155c6cbf7a024c098e8ac241003a921f455dcbf
nvanacoleyen <n.h.vanacoleyen@student.tudelft.nl>
2025-02-11 16:59:35 +0100

    Add some working version of starburst generator, also added opencv

--- LensFlare_CGSeminar/src/application.cpp
+++ LensFlare_CGSeminar/src/application.cpp
@@ -26,6 +26,7 @@ DISABLE_WARNINGS_POP()
 #include "camera.h"
 #include "utils.h"
 #include "preset_lens_systems.h"
+#include <opencv2/opencv.hpp>
 
 constexpr int FULL_WIDTH = 1920;
 constexpr int HEIGHT = 1080;
@@ -40,6 +41,136 @@ struct QuadData {
     GLuint quadID;
     float intensityVal;
 };
+
+
+cv::Vec3f wavelengthToRGB(double wavelength) {
+    double R = 0.0, G = 0.0, B = 0.0;
+
+    if (wavelength >= 380.0 && wavelength <= 440.0) {
+        R = -1.0 * (wavelength - 440.0) / (440.0 - 380.0);
+        G = 0.0;
+        B = 1.0;
+    }
+    else if (wavelength > 440.0 && wavelength <= 490.0) {
+        R = 0.0;
+        G = (wavelength - 440.0) / (490.0 - 440.0);
+        B = 1.0;
+    }
+    else if (wavelength > 490.0 && wavelength <= 510.0) {
+        R = 0.0;
+        G = 1.0;
+        B = -1.0 * (wavelength - 510.0) / (510.0 - 490.0);
+    }
+    else if (wavelength > 510.0 && wavelength <= 580.0) {
+        R = (wavelength - 510.0) / (580.0 - 510.0);
+        G = 1.0;
+        B = 0.0;
+    }
+    else if (wavelength > 580.0 && wavelength <= 645.0) {
+        R = 1.0;
+        G = -1.0 * (wavelength - 645.0) / (645.0 - 580.0);
+        B = 0.0;
+    }
+    else if (wavelength > 645.0 && wavelength <= 750.0) {
+        R = 1.0;
+        G = 0.0;
+        B = 0.0;
+    }
+
+    // Let the intensity fall off near the vision limits
+    double factor = 0.0;
+    if (wavelength >= 380.0 && wavelength <= 420.0) {
+        factor = 0.3 + 0.7 * (wavelength - 380.0) / (420.0 - 380.0);
+    }
+    else if (wavelength > 420.0 && wavelength <= 700.0) {
+        factor = 1.0;
+    }
+    else if (wavelength > 700.0 && wavelength <= 750.0) {
+        factor = 0.3 + 0.7 * (750.0 - wavelength) / (750.0 - 700.0);
+    }
+
+    R *= factor;
+    G *= factor;
+    B *= factor;
+
+    return cv::Vec3f(R, G, B);
+}
+
+
+int createStarburst() {
+    // Load the aperture texture
+    cv::Mat aperture = cv::imread("resources/star_shaped_aperture.png", cv::IMREAD_GRAYSCALE);
+    if (aperture.empty()) {
+        std::cerr << "Failed to load aperture texture." << std::endl;
+        return -1;
+    }
+
+    // Expand to optimal size for DFT
+    int m = cv::getOptimalDFTSize(aperture.rows);
+    int n = cv::getOptimalDFTSize(aperture.cols);
+    cv::copyMakeBorder(aperture, aperture, 0, m - aperture.rows, 0, n - aperture.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+
+    // Perform DFT
+    cv::Mat planes[] = { cv::Mat_<float>(aperture), cv::Mat::zeros(aperture.size(), CV_32F) };
+    cv::Mat complexI;
+    cv::merge(planes, 2, complexI);
+    cv::dft(complexI, complexI);
+
+    // Compute the magnitude (power spectrum)
+    cv::split(complexI, planes);
+    cv::magnitude(planes[0], planes[1], planes[0]);
+    cv::Mat magnitudeImage = planes[0];
+
+    // Rearrange the quadrants of Fourier image
+    int cx = magnitudeImage.cols / 2;
+    int cy = magnitudeImage.rows / 2;
+
+    cv::Mat q0(magnitudeImage, cv::Rect(0, 0, cx, cy));
+    cv::Mat q1(magnitudeImage, cv::Rect(cx, 0, cx, cy));
+    cv::Mat q2(magnitudeImage, cv::Rect(0, cy, cx, cy));
+    cv::Mat q3(magnitudeImage, cv::Rect(cx, cy, cx, cy));
+
+    cv::Mat tmp;
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+
+    cv::normalize(magnitudeImage, magnitudeImage, 0, 1, cv::NORM_MINMAX);
+
+    // Prepare an RGB texture by superimposing scaled spectra for different wavelengths
+    cv::Mat starburstTexture = cv::Mat::zeros(magnitudeImage.size(), CV_32FC3);
+
+    // Wavelengths from 380 nm to 750 nm (visible spectrum)
+    for (double lambda = 380.0; lambda <= 750.0; lambda += 5.0) {
+        double scale = 1/* Define your scaling based on wavelength */;
+        cv::Mat scaledMagnitude;
+        cv::multiply(magnitudeImage, scale, scaledMagnitude);
+
+        // Convert wavelength to RGB
+        cv::Vec3f color = wavelengthToRGB(lambda);
+
+        // Accumulate into the starburst texture
+        for (int y = 0; y < starburstTexture.rows; ++y) {
+            for (int x = 0; x < starburstTexture.cols; ++x) {
+                float intensity = scaledMagnitude.at<float>(y, x);
+                starburstTexture.at<cv::Vec3f>(y, x) += intensity * color;
+            }
+        }
+    }
+
+    // Normalize the final texture
+    cv::normalize(starburstTexture, starburstTexture, 0, 1, cv::NORM_MINMAX);
+
+
+    // Save the starburst texture
+    cv::imshow("starburst_texture.png", starburstTexture);
+
+    return 0;
+}
 
 
 class Application {
@@ -109,6 +240,7 @@ public:
     void update()
     {
         /* INIT */
+        createStarburst();
 
         /* LIGHT SPHERE */
         const Mesh lightSphere = mergeMeshes(loadMesh("resources/sphere.obj"));
